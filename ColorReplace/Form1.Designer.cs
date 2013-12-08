404cb018ccf309bb396fdcf2378b429b0d9bb062
inboots <yahch@live.com>
2013-12-08 14:59:20 +0800

    first commit

---
+++ ColorReplace/Form1.Designer.cs
@@ -0,0 +1,162 @@
+﻿namespace ColorReplace
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.button4 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(29, 12);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "文件";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(13, 29);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(197, 21);
+            this.textBox1.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(216, 28);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(44, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "...";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(15, 238);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(126, 23);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "指定颜色转透明色";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(13, 60);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(65, 12);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "颜色ARGB值";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(84, 57);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(125, 21);
+            this.textBox3.TabIndex = 6;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(216, 58);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(45, 23);
+            this.button3.TabIndex = 7;
+            this.button3.Text = "Add";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 12;
+            this.listBox1.Location = new System.Drawing.Point(15, 84);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(245, 148);
+            this.listBox1.TabIndex = 8;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(148, 239);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(112, 23);
+            this.button4.TabIndex = 9;
+            this.button4.Text = "非透明色转指定色";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(273, 278);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "ColorReplace";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button button4;
+    }
+}
+
---
+++ ColorReplace/Form1.cs
@@ -0,0 +1,200 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace ColorReplace
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private string img_file = string.Empty;
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+
+
+        /// <summary>
+        /// 将图像中某些颜色转换为透明
+        /// </summary>
+        /// <param name="img">图片</param>
+        /// <param name="colors">要转换为透明的颜色</param>
+        private Bitmap MakeImageTransparent(Image img, List<Color> colors)
+        {
+            Bitmap bmp = (Bitmap)img;
+            Bitmap newBmp = new Bitmap(bmp.Width, bmp.Height);
+            for (int x = 0; x < bmp.Width; x++)
+            {
+                for (int y = 0; y < bmp.Height; y++)
+                {
+                    Color pix = bmp.GetPixel(x, y);
+                    if (colors.Contains(pix))
+                    {
+                        newBmp.SetPixel(x, y, Color.Transparent);
+                    }
+                    else
+                    {
+                        newBmp.SetPixel(x, y, pix);
+                    }
+                }
+            }
+            return newBmp;
+        }
+
+        /// <summary>
+        /// 如果不是透明色，用其他颜色代替
+        /// </summary>
+        /// <param name="img"></param>
+        /// <param name="color"></param>
+        /// <returns></returns>
+        private Bitmap MakeOtherColorIfNotTransparent(Image img, Color color)
+        {
+            Bitmap bmp = (Bitmap)img;
+
+            Bitmap newBmp = new Bitmap(bmp.Width, bmp.Height);
+            for (int x = 0; x < bmp.Width; x++)
+            {
+                for (int y = 0; y < bmp.Height; y++)
+                {
+                    Color pix = bmp.GetPixel(x, y);
+                    if (pix != Color.FromArgb(0,255,255,255))
+                    {
+                        newBmp.SetPixel(x, y, color);
+                    }
+                    else
+                    {
+                        newBmp.SetPixel(x, y, Color.Transparent);
+                    }
+                }
+            }
+            return newBmp;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (this.img_file == string.Empty)
+            {
+                MessageBox.Show("请选择文件！");
+            }
+            else
+            {
+                Image img = Image.FromFile(img_file);
+                List<Color> colors = new List<Color>();
+                if (listBox1.Items.Count > 0)
+                {
+                    for (int i = 0; i < listBox1.Items.Count; i++)
+                    {
+                        string line = listBox1.Items[i].ToString();
+                        string[] arr = line.Split(new char[] { ',' });
+                        int c1 = int.Parse(arr[0]);
+                        int c2 = int.Parse(arr[1]);
+                        int c3 = int.Parse(arr[2]);
+                        int c4 = int.Parse(arr[3]);
+                        Color cr = Color.FromArgb(c1, c2, c3, c4);
+                        colors.Add(cr);
+                    }
+                    Bitmap bmpnew = MakeImageTransparent(img, colors);
+                    string filename = DateTime.Now.Ticks.ToString() + ".bmp";
+                    bmpnew.Save(filename);
+                    MessageBox.Show("ok!");
+                }
+                else
+                {
+                    MessageBox.Show("没有指定颜色");
+                }
+
+
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.Filter = "图片|*.jpg";
+            if (ofd.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                this.img_file = ofd.FileName;
+                textBox1.Text = this.img_file;
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            string[] s = textBox3.Text.Split(new char[] { ',' });
+            bool ts = true;
+            if (s.Length == 4)
+            {
+                for (int i = 0; i < s.Length; i++)
+                {
+                    try
+                    {
+                        int.Parse(s[i]);
+                    }
+                    catch
+                    {
+                        ts = false;
+                    }
+                }
+            }
+            else
+            {
+                ts = false;
+            }
+            if (!ts)
+            {
+                MessageBox.Show("无效的ARGB格式");
+            }
+            else
+            {
+                listBox1.Items.Add(textBox3.Text);
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            if (this.img_file == string.Empty)
+            {
+                MessageBox.Show("请选择文件！");
+            }
+            else
+            {
+                Image img = Image.FromFile(img_file);
+                List<Color> colors = new List<Color>();
+                if (listBox1.Items.Count > 0)
+                {
+
+                    string line = listBox1.Items[0].ToString();
+                    string[] arr = line.Split(new char[] { ',' });
+                    int c1 = int.Parse(arr[0]);
+                    int c2 = int.Parse(arr[1]);
+                    int c3 = int.Parse(arr[2]);
+                    int c4 = int.Parse(arr[3]);
+                    Color cr = Color.FromArgb(c1, c2, c3, c4);
+
+
+                    Bitmap bmpnew = MakeOtherColorIfNotTransparent(img, cr);
+                    string filename = DateTime.Now.Ticks.ToString() + ".bmp";
+                    bmpnew.Save(filename);
+                    MessageBox.Show("ok!");
+                }
+                else
+                {
+                    MessageBox.Show("没有指定颜色");
+                }
+
+
+            }
+        }
+    }
+}
